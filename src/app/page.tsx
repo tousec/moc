c71c61ad5269da3c4e28203c9aaab18daa49e0ef
tousec <toufiquer.sec@gmail.com>
2025-06-26 17:19:58 +0600

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+import { Button } from "@/components/ui/button";
+
+ 
+export default function Home() {
+  return (
+    <div>Just demo 2.2 <Button variant="outline">New Update </Button> </div>
+  );
+}
