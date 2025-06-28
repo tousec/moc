61f841c1fdd24b532c5438affd380760d8322b9a
tousec <toufiquer.sec@gmail.com>
2025-06-28 18:47:16 +0600

    done @ update pwa and add some file for dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,3 @@
-import CourseCorousel from "@/components/common/course-corousel/course-corousel";
-import CompleteHeroComponent from "@/components/common/home-page-component"; 
-import { MainCarousel } from "@/components/common/main-carousel/main-carousel";
-
-
- 
 export default function Home() {
-  return (
-    <div className="w-full max-w-7xl mx-auto">
-      <div className="z-10">
-      <MainCarousel />
-        <CourseCorousel />
-        <div className="mt-2"/>
-    <CompleteHeroComponent />
-      </div>
-    </div>
-  );
+  return <div>Project 1.2</div>;
 }
